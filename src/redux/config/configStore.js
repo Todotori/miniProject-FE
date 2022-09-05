51f28d4dce5aa4b0665f84cd4eccc9b8887a5407
Hwang-Junsu <hjs77728@gmail.com>
2022-09-05 18:09:42 +0900

    [Fix] tagSlice

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -3,16 +3,18 @@ import signinUser from "../modules/signinUserSlice";
 import checkEmail from "../modules/checkEmailSlice";
 import checkUsername from "../modules/checkUsernameSlice";
 import todos from "../modules/todos";
+import tags from "../modules/tagSlice";
 import {configureStore} from "@reduxjs/toolkit";
 
 const store = configureStore({
-    reducer: {
-        checkUsername,
-        checkEmail,
-        signinUser,
-        createUser,
-        todos,
-    },
+  reducer: {
+    checkUsername,
+    checkEmail,
+    signinUser,
+    createUser,
+    todos,
+    tags,
+  },
 });
 
 export default store;
