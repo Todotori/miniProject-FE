37d75fe645f128fa8eefb41eb2f3965888aa5d80
MIINII <ft.dawn.o@gmail.com>
2022-09-05 10:40:31 +0900

    [MAIN] Dropdown

--- src/pages/Home/AddTodo.jsx
+++ src/pages/Home/AddTodo.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { BsPencilSquare } from 'react-icons/bs';
-import Multiselect from 'multiselect-react-dropdown';
 import AddModal from './AddModal.jsx';
 
+import Dropdown from '../../component/Layout/Dropdown.js';
+
 const AddTodo = () => {
-  const data = [
-    { id: 0, title: '우리가좍' },
-    { id: 1, title: '상수리나무' },
-    { id: 2, title: '프론트엔드' },
-  ];
   const [isOpen, setIsOpen] = React.useState(false);
   const modalIsOpen = () => {
     setIsOpen(true);
@@ -22,25 +18,7 @@ const AddTodo = () => {
         <button onClick={modalIsOpen}>할 일을 추가하세요</button>
       </AddTitle>
       {isOpen && <AddModal setIsOpen={setIsOpen} />}
-      <Multiselect
-        displayValue='title'
-        onKeyPressFn={function noRefCheck() {}}
-        onRemove={function noRefCheck() {}}
-        onSearch={function noRefCheck() {}}
-        onSelect={function noRefCheck() {}}
-        options={data}
-        showCheckbox
-        showArrow
-        placeholder=''
-        style={{
-          searchBox: {
-            border: '1px solid teal',
-            'min-width': '200px',
-            'max-width': '350px',
-            height: 'auto',
-          },
-        }}
-      />
+      <Dropdown />
     </TodoWrap>
   );
 };
@@ -61,4 +39,3 @@ const AddTitle = styled.div`
 `;
 
 export default AddTodo;
-
