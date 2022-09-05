f3d111217d64ec291cce176b0a06068580ec7163
MIINII <ft.dawn.o@gmail.com>
2022-09-05 16:14:20 +0900

    🚧 작업중입니다

--- src/redux/modules/checkSlice.js
+++ src/redux/modules/checkSlice.js
@@ -1,1 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
+import api from '../../../db.json';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  todos: [],
+  error: null,
+  isLoading: false,
+};
+
+export const __getTodos = createAsyncThunk('todos/getTodos', async (payload, thunkAPI) => {
+  try {
+    const { data } = await api.get('/todo');
+    return thunkAPI.fulfillWithValue(data);
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+const checkSlice = createSlice({
+  name: 'check',
+  initialState,
+  reducers: {},
+});
