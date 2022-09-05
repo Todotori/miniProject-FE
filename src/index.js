a10185d2485486931d891b2132a000c78e0eaa29
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-05 12:16:33 +0900

    [Update] Move configStore.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./pages/styles/GlobalStyle.js";
 import {Provider} from "react-redux";
-import store from "./redux/configStore";
+import store from "./redux/config/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <GlobalStyle />
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </>
+    <>
+        <GlobalStyle/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </>
 );
