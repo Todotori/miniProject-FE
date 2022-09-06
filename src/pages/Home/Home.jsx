606650344b5adbb7063a7ab3f21975e8f4dbf0c4
MIINII <ft.dawn.o@gmail.com>
2022-09-06 21:23:32 +0900

    화이팅

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,27 +6,27 @@ import AddTodo from './AddTodo.jsx';
 import MyTodos from '../../component/Mypage/MyTodos';
 
 const Home = () => {
-    // const [category, setCategory] = useState('all')
-    console.log(sessionStorage.getItem("current_user"));
-    // const categories = ['All', ...new Set(items.map(item => item.company))];
+  // const [category, setCategory] = useState('all')
+  console.log(sessionStorage.getItem('current_user'));
+  // const categories = ['All', ...new Set(items.map(item => item.company))];
 
-    return (
-        <Layout>
-            <Main>
-                {/* ANCHOR : 02. mainWRap */}
-                <MainWrap>
-                    {/* 02-1. Header */}
-                    <Welcome/>
+  return (
+    <Layout>
+      <Main>
+        {/* ANCHOR : 02. mainWRap */}
+        <MainWrap>
+          {/* 02-1. Header */}
+          <Welcome />
 
-                    {/* SECTION : 02-2. wrapTodo */}
-                    <AddTodo/>
+          {/* SECTION : 02-2. wrapTodo */}
+          <AddTodo />
 
-                    {/* ANCHOR : 02-3 : todoList */}
-                    <MyTodos title={'Todolist'}/>
-                </MainWrap>
-            </Main>
-        </Layout>
-    );
+          {/* ANCHOR : 02-3 : todoList */}
+          <MyTodos title={'Todolist'} />
+        </MainWrap>
+      </Main>
+    </Layout>
+  );
 };
 
 const Main = styled.div`
