f93edef11b849cb93cbc493a60fb93569135d9f6
Hwang-Junsu <hjs77728@gmail.com>
2022-09-07 21:23:38 +0900

    Temp

--- src/redux/modules/userInfoSlice.js
+++ src/redux/modules/userInfoSlice.js
@@ -2,18 +2,17 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import api from "../../api";
 
 const initialState = {
-  isLoading: false,
+  success: false,
+  data: {nickname: "", email: "", company: ""},
   error: null,
-  response: null,
 };
 
 export const __getUserInfo = createAsyncThunk(
   "users/getUserInfo",
   async (user, thunk) => {
     try {
-      const response = await api.get(`/member/${user}`);
-      console.log(response);
-      // return thunk.fulfillWithValue(data);
+      const response = await api.get(`/member`);
+      return thunk.fulfillWithValue(response.data);
     } catch (error) {
       return thunk.rejectWithValue(error);
     }
@@ -26,7 +25,7 @@ const userInfoSlice = createSlice({
   reducers: {},
   extraReducers: {
     [__getUserInfo.fulfilled]: (state, action) => {
-      console.log(action);
+      return (state = action.payload);
     },
     [__getUserInfo.rejected]: (state, action) => {
       console.log(state, action);
