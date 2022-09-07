2c006d455481a52b1242c999f1562b5e28aed72a
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-07 20:30:28 +0900

    [Reset] User 데이터베이스 초기화.

--- src/redux/modules/signInUserSlice.js
+++ src/redux/modules/signInUserSlice.js
@@ -2,53 +2,52 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import api from "../../api";
 
 const initialState = {
-  isLoading: false,
-  error: null,
-  response: null,
+    isLoading: false,
+    error: null,
+    response: null,
 };
 
 export const signInUserThunk = createAsyncThunk(
-  "users/signInUser",
-  async (user, thunk) => {
-    try {
-      const response = await api.post("/login", user);
-      console.dir(response);
-      const {data} = response;
-      if (data.success) {
-        if (!response.headers["authorization"]) {
-          return thunk.rejectWithValue("TOKEN_NOT_SENT");
+    "users/signInUser",
+    async (user, thunk) => {
+        try {
+            const response = await api.post("/login", user);
+            const {data} = response;
+            if (data.success) {
+                if (!response.headers["authorization"]) {
+                    return thunk.rejectWithValue("TOKEN_NOT_SENT");
+                }
+                const token = response.headers["authorization"].split(" ")[1];
+                const userID = data.data.id;
+                return thunk.fulfillWithValue({token, userID});
+            } else {
+                const {code} = data.error;
+                return thunk.rejectWithValue(code);
+            }
+        } catch (error) {
+            return thunk.rejectWithValue(error);
         }
-        const token = response.headers["authorization"].split(" ")[1];
-        const userID = data.data.id;
-        return thunk.fulfillWithValue({token, userID});
-      } else {
-        const {code} = data.error;
-        return thunk.rejectWithValue(code);
-      }
-    } catch (error) {
-      return thunk.rejectWithValue(error);
     }
-  }
 );
 
 const signInUserSlice = createSlice({
-  name: "signinUser",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(signInUserThunk.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(signInUserThunk.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.response = action.payload;
-      })
-      .addCase(signInUserThunk.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
-  },
+    name: "signinUser",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(signInUserThunk.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(signInUserThunk.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.response = action.payload;
+            })
+            .addCase(signInUserThunk.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
 export default signInUserSlice.reducer;
