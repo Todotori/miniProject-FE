356f9220a8541525ecad94ef9e65e465c8479558
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2022-09-07 15:24:45 +0900

    [Update] 로그인되어있지 않을 시 로그인 화면으로 넘어가게 수정.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
 import Layout from "../../component/Layout/Layout";
 import Welcome from "./Welcome";
 import AddTodo from "./AddTodo.jsx";
 import MyTodos from "../../component/Mypage/MyTodos";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
-  // const [category, setCategory] = useState('all')
+    // const [category, setCategory] = useState('all')
 
-  // const categories = ['All', ...new Set(items.map(item => item.company))];
+    // const categories = ['All', ...new Set(items.map(item => item.company))];
+    const navigator = useNavigate();
+    useEffect(() => {
+        const token = sessionStorage.getItem("access_token");
+        if (!token) {
+            navigator("/login");
+        }
+    }, []);
+    return (
+        <Layout>
+            <Main>
+                {/* ANCHOR : 02. mainWRap */}
+                <MainWrap>
+                    {/* 02-1. Header */}
+                    <Welcome/>
 
-  return (
-    <Layout>
-      <Main>
-        {/* ANCHOR : 02. mainWRap */}
-        <MainWrap>
-          {/* 02-1. Header */}
-          <Welcome />
+                    {/* SECTION : 02-2. wrapTodo */}
+                    <AddTodo/>
 
-          {/* SECTION : 02-2. wrapTodo */}
-          <AddTodo />
-
-          {/* ANCHOR : 02-3 : todoList */}
-          <MyTodos title={"Todolist"} />
-        </MainWrap>
-      </Main>
-    </Layout>
-  );
+                    {/* ANCHOR : 02-3 : todoList */}
+                    <MyTodos title={"Todolist"}/>
+                </MainWrap>
+            </Main>
+        </Layout>
+    );
 };
 
 const Main = styled.div`
