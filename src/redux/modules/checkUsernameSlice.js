4b573da898d637ec50148fb2983e94be35e72788
Hwang-Junsu <hjs77728@gmail.com>
2022-09-08 20:38:25 +0900

    MyPage

--- src/redux/modules/checkUsernameSlice.js
+++ src/redux/modules/checkUsernameSlice.js
@@ -2,35 +2,41 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import api from "../../api";
 
 const initialState = {
-    isLoading: false,
-    error: null,
-    response: null
-}
+  isLoading: false,
+  error: null,
+  response: null,
+};
 
-export const checkUsernameThunk = createAsyncThunk("users/checkUsername", async (username, thunk) => {
+export const checkUsernameThunk = createAsyncThunk(
+  "users/checkUsername",
+  async (username, thunk) => {
     try {
-        const {data} = await api.post("http://3.34.90.63/api/nickcheck", {nickname: username});
-        return thunk.fulfillWithValue(data);
+      const {data} = await api.post("/api/nickcheck", {nickname: username});
+      return thunk.fulfillWithValue(data);
     } catch (error) {
-        return thunk.rejectWithValue(error);
+      return thunk.rejectWithValue(error);
     }
-})
+  }
+);
 
 const checkUsernameSlice = createSlice({
-    name: "checkUsername",
-    initialState,
-    reducers: {},
-    extraReducers: builder => {
-        builder.addCase(checkUsernameThunk.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(checkUsernameThunk.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.response = action.payload;
-        }).addCase(checkUsernameThunk.rejected, (state, action) => {
-            state.isLoading = false;
-            state.error = action.payload;
-        })
-    }
-})
+  name: "checkUsername",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(checkUsernameThunk.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkUsernameThunk.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.response = action.payload;
+      })
+      .addCase(checkUsernameThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
+});
 
 export default checkUsernameSlice.reducer;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -2,164 +2,167 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import api from "../../api";
 
 const initialState = {
-    todos: [],
-    count: 0,
-    error: null,
-    isLoading: false,
+  todos: [],
+  count: 0,
+  error: null,
+  isLoading: false,
 };
 
 export const __getTodos = createAsyncThunk(
-    "todos/getTodos",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.get("http://3.34.90.63/api/todo/all");
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "todos/getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.get("/todo/all");
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 export const __getMyTodos = createAsyncThunk(
-    //로그인 상태에서만 가능.
-    "todos/getMyTodos",
-    async (payload, thunkAPI) => {
-        try {
-            let data;
-            switch (payload) {
-                case "WHOLE":
-                    const WholeTodos = await api.get("http://3.34.90.63/api/todo/");
-                    data = WholeTodos.data;
-                    break;
-                case "TODO":
-                    const Todos = await api.get("/todo/undone");
-                    data = Todos.data;
-                    break;
-                case "DONE":
-                    const Dones = await api.get("/todo/done");
-                    data = Dones.data;
-                    break;
-            }
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  //로그인 상태에서만 가능.
+  "todos/getMyTodos",
+  async (payload, thunkAPI) => {
+    try {
+      let data;
+      switch (payload) {
+        case "WHOLE":
+          const WholeTodos = await api.get("/api/todo/");
+          data = WholeTodos.data;
+          break;
+        case "TODO":
+          const Todos = await api.get("/todo/undone");
+          data = Todos.data;
+          break;
+        case "DONE":
+          const Dones = await api.get("/todo/done");
+          data = Dones.data;
+          break;
+      }
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const __getTodosCount = createAsyncThunk(
-    "todos/getTodosCount",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.get("http://3.34.90.63/api/todo/undone");
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "todos/getTodosCount",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.get("http://3.34.90.63/api/todo/undone");
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const __addTodo = createAsyncThunk(
-    "todos/addTodo",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.post("http://3.34.90.63/api/todo", payload);
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "todos/addTodo",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.post("http://3.34.90.63/api/todo", payload);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const __editTodo = createAsyncThunk(
-    "todos/editTodo",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.put("http://3.34.90.63/api/todo", payload);
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "todos/editTodo",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.put("http://3.34.90.63/api/todo", payload);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const __deleteTodo = createAsyncThunk(
-    "todos/deleteTodo",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.delete(`http://3.34.90.63/api/todo/${payload}`);
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
-        }
+  "todos/deleteTodo",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.delete(`http://3.34.90.63/api/todo/${payload}`);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const __updateIsDone = createAsyncThunk(
-    "todos/updateIsDone",
-    async (payload, thunkAPI) => {
-        try {
-            const {data} = await api.put(`http://3.34.90.63/api/todo/${payload}/done`, {
-                id: payload,
-                done: true,
-            });
-            return thunkAPI.fulfillWithValue(data);
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error);
+  "todos/updateIsDone",
+  async (payload, thunkAPI) => {
+    try {
+      const {data} = await api.put(
+        `http://3.34.90.63/api/todo/${payload}/done`,
+        {
+          id: payload,
+          done: true,
         }
+      );
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
     }
+  }
 );
 
 export const todoSlice = createSlice({
-    name: "todos",
-    initialState,
-    reducers: {},
-    extraReducers: {
-        [__getTodos.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [__getTodos.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.todos = action.payload.data;
-        },
-        [__getTodos.rejected]: (state, action) => {
-            state.isLoading = false;
-            state.error = action.payload;
-        },
-        [__getMyTodos.fulfilled]: (state, action) => {
-            state.todos = action.payload.data;
-        },
-
-        [__addTodo.fulfilled]: (state, action) => {
-            state.todos = [action.payload.data, ...state.todos];
-        },
-        [__addTodo.rejected]: (state, action) => {
-            return;
-        },
-
-        [__updateIsDone.fulfilled]: (state, action) => {
-            state.todos.map((todo) => {
-                if (todo.id === action.payload.data.id) {
-                    return (todo.done = action.payload.data.done);
-                } else {
-                    return todo;
-                }
-            });
-        },
-        [__updateIsDone.rejected]: (state, action) => {
-            state.error = action.payload;
-        },
-        [__deleteTodo.fulfilled]: (state, action) => {
-            if (!action.payload.success) {
-                return;
-            }
-            state.todos = state.todos.filter((todo) => todo.id !== action.meta.arg);
-            return state;
-        },
-
-        [__getTodosCount.fulfilled]: (state, action) => {
-            state.count = action.payload.data.length;
-        },
+  name: "todos",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [__getTodos.pending]: (state) => {
+      state.isLoading = true;
     },
+    [__getTodos.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.todos = action.payload.data;
+    },
+    [__getTodos.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [__getMyTodos.fulfilled]: (state, action) => {
+      state.todos = action.payload.data;
+    },
+
+    [__addTodo.fulfilled]: (state, action) => {
+      state.todos = [action.payload.data, ...state.todos];
+    },
+    [__addTodo.rejected]: (state, action) => {
+      return;
+    },
+
+    [__updateIsDone.fulfilled]: (state, action) => {
+      state.todos.map((todo) => {
+        if (todo.id === action.payload.data.id) {
+          return (todo.done = action.payload.data.done);
+        } else {
+          return todo;
+        }
+      });
+    },
+    [__updateIsDone.rejected]: (state, action) => {
+      state.error = action.payload;
+    },
+    [__deleteTodo.fulfilled]: (state, action) => {
+      if (!action.payload.success) {
+        return;
+      }
+      state.todos = state.todos.filter((todo) => todo.id !== action.meta.arg);
+      return state;
+    },
+
+    [__getTodosCount.fulfilled]: (state, action) => {
+      state.count = action.payload.data.length;
+    },
+  },
 });
 
 export default todoSlice.reducer;
