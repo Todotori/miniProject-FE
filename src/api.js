e49c811cc0e606cf6d67a18e9f65e36a9d89326d
Hwang-Junsu <hjs77728@gmail.com>
2022-09-08 20:20:57 +0900

    Temp

--- src/api.js
+++ src/api.js
@@ -4,13 +4,13 @@ const token = sessionStorage.getItem("access_token");
 let api;
 
 if (token) {
-    const bearerToken = "Bearer " + token;
-    api = axios.create({
-        baseURL: process.env.API_URL,
-        headers: {authorization: bearerToken},
-    });
+  const bearerToken = "Bearer " + token;
+  api = axios.create({
+    baseURL: "http://3.34.90.63/api",
+    headers: {authorization: bearerToken},
+  });
 } else {
-    api = axios.create({baseURL: process.env.API_URL});
+  api = axios.create({baseURL: "http://3.34.90.63/api"});
 }
 
 export default api;
